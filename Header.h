c3f00d5f4fad9f37c030a7abeb5754e290f406aa
Khubashvili-Luka <113700471+Khubashvili-Luka@users.noreply.github.com>
2023-01-19 14:05:22 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class game_Khubashvili
+{
+private:
+    int count_players;
+    string enemy = "";
+    int phase;
+    int year;
+    struct player
+    {
+        string name;
+        int ochered_hoda;
+        int gold;
+        int wood;
+        int rock;
+        int coins;
+        int cubic1;
+        int cubic2;
+        int cubic3;
+        int additional_cubic;
+        bool adviser;
+        int reestr;
+        int points;
+        vector<int> buildings;
+    };
+
+public:
+    game_Khubashvili() {};
+    game_Khubashvili(int count_players) {};
+    game_Khubashvili() {};
+    void phase1() {};
+    void phase2() {};
+    void phase3() {};
+    void phase4() {};
+    void phase5() {};
+    void phase6() {};
+    void phase7() {};
+    void phase8() {};
+    void defense_level(const char* enemy_name, int king_help, SomeDataStructForPlayer& player) {};
+
+};
